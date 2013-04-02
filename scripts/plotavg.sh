1f4e81f0c5cc9330d21c937125215c35f8cd938c
Riccardo Cefala <riccardo.cefala@gmail.com>
2013-04-02 11:53:44 +0200

    Fix path to R script for generating plots

--- scripts/plotavg.sh
+++ scripts/plotavg.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 RTERM=$(which R)
+PLOTS=$(dirname $0)/plotavg.R
 
 TESTDIR=$1
 
 [ -z "$TESTDIR" ]  && echo "$0 <TEST_DIR>" && exit 1
    
-$RTERM --no-restore --no-save --args $TESTDIR < plotavg.R > R.log 2>&1
+$RTERM --no-restore --no-save --args $TESTDIR < $PLOTS > R.log 2>&1
